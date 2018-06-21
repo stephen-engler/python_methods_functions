8df0eb5e6817267c6190539717c7ca694fc71eac
Stephen Engler <sengler1989@gmail.com>
2018-06-21 15:11:04 -0500

    pig latin function

--- functions.py
+++ functions.py
@@ -20,4 +20,16 @@ print(add(3,2))
 #find if dog is in a string
 
 def dog_check(mystring):
-    return 'dog' in mystring.lower():
+    return 'dog' in mystring.lower()
+
+def pig_latin(word):
+    first_letter = word[0]
+
+    #check if vowel
+    if first_letter in 'aeiou':
+        pig_word = word + 'ay'
+    else:
+        pig_word = word[1:] + first_letter + 'ay'
+    return pig_word
+
+print(pig_latin('apple'))
